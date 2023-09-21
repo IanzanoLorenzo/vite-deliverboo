0f14535a68db9f3e8664eb726aaab4a3bc24eec7
SimoneVilardo <simovila95@gmail.com>
2023-09-21 18:11:23 +0200

    carrello

--- src/pages/AppMenu.vue
+++ src/pages/AppMenu.vue
@@ -6,11 +6,23 @@ export default {
         return {
             store,
             axios,
-            dishes: ''
+            dishes: '',
+            cart: [],
+            priceTotal: 0
         }
     },
     mounted() {
+        const storedCart = localStorage.getItem('cart');
+
+        if (storedCart) {
+            this.cart = JSON.parse(storedCart);
+        }
+
         this.getDishes(this.$route.params.slug);
+
+        this.getDishes(this.$route.params.slug);
+
+        this.totalPrice()
     },
     methods: {
         getDishes(slug){
@@ -18,13 +30,75 @@ export default {
                 this.dishes = response.data.response;
             }
             )
+        },
+        addToCart(dish) {
+        // Verifica se il piatto è già nel carrello
+        const existingDish = this.cart.find(item => item.id === dish.id);
+
+        if (existingDish) {
+            // Se il piatto è già nel carrello, incrementa la quantità
+            existingDish.quantity++;
+        } else {
+            // Se il piatto non è nel carrello, aggiungilo
+            this.cart.push({
+                id: dish.id,
+                name: dish.name,
+                price: dish.price,
+                quantity: 1, // Inizia con una quantità di 1
+            });
+        }
+
+        // Salva il carrello in localStorage per la persistenza
+        localStorage.setItem('cart', JSON.stringify(this.cart));
+        this.totalPrice();
+        },
+
+        removeFromCart(dish , index){
+
+            // Verifica se il piatto è già nel carrello
+            const existingDish = this.cart.find(item => item.id === dish.id);
+
+            if(existingDish && existingDish.quantity > 1){
+                // Se il piatto è già nel carrello e non è l'ultimo piatto, decrementa la quantità
+                existingDish.quantity--;
+            }else if (existingDish && existingDish.quantity === 1){
+                // Se il piatto è l'ultimo nel carrello cancella il piatto
+                this.cart.splice(index, 1)
+            }
+            // Salva il carrello il localStorage
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            this.totalPrice();
+        },
+        totalPrice(){
+            this.priceTotal = 0
+            this.cart.forEach(dish => {
+                const totalDishPrice = dish.price * dish.quantity
+                this.priceTotal += totalDishPrice 
+            });
+            this.priceTotal = Math.round((this.priceTotal) * 100) / 100
         }
     },
 }
 </script>
 <template>
     <div>
-        <div v-for="dish in dishes" :key="dish.id">{{ dish.name }}</div>
+        <div class="d-flex" v-for="dish in dishes" :key="dish.id">
+            <p>{{ dish.name }}</p>
+            <p>{{ dish.price }}€</p>
+            <button @click="addToCart(dish)">Aggiungi al carrello</button>
+        </div>
+    </div>
+    <div>
+        <h2>Carrello</h2>
+        <div class="d-flex" v-for="(item, index) in cart" :key="item.id">
+            <p>{{ item.name }}</p>
+            <p>{{ item.price }}€</p>
+            <p>Quantità: {{ item.quantity }}</p>
+            <button @click="removeFromCart(item , index)">Rimuovi</button>
+        </div>
+        <div>
+          Prezzo Totale:  {{ priceTotal }}€
+        </div>
     </div>
 </template>
 <style lang="scss">
