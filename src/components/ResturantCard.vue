1b702edf21b5d28c175dda41a65263a2afa68272
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-21 13:42:54 +0200

    create card ristoranti momentanee

--- src/components/ResturantCard.vue
+++ src/components/ResturantCard.vue
@@ -1,13 +1,42 @@
 <script>
+import { store } from '../store';
 export default {
-    
+    data() {
+        return {
+            store
+        }
+    },
+    props:{
+        resturant: Object
+    },
 }
 </script>
 <template>
-    <div>
-        
+    <div class="card" style="width: 100%;">
+        <img v-if="resturant.cover_image === null" :src="`${store.basicUrl}/storage/cover_images/default.png`" class="img-fluid">
+        <img v-else :src="`${store.basicUrl}storage/${resturant.cover_image}`" class="img-fluid">
+        <div class="card-body">
+            <h5 class="card-title">{{ resturant.name }}</h5>
+            <p class="card-text d-none d-md-block">{{ resturant.address }}</p>
+            <span class="badge bg-primary mx-1" v-for="type in resturant.types" :key="type.id">{{ type.name }}</span>
+        </div>
     </div>
 </template>
-<style lang="scss">
-    
+<style lang="scss" scoped>
+.card {
+    overflow: hidden;
+    border-radius: 15px !important;
+    .img-fluid{
+        height: 200px;
+        object-fit: cover;
+    }
+    p.card-text{
+    height: 100px;
+    overflow: hidden;
+    }
+    h5.card-title{
+        height: 40px;
+        overflow: hidden;
+    }
+}
 </style>
--- src/pages/AppResturant.vue
+++ src/pages/AppResturant.vue
@@ -1,7 +1,11 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import ResturantCard from '../components/ResturantCard.vue';
 export default {
+    components:{
+        ResturantCard
+    },
     data() {
         return {
             store,
@@ -15,9 +19,10 @@ export default {
     methods: {
         //CHIAMATA AXIOS
         getResturants(){
-           if(store.resturants){
-            axios.get('http://127.0.0.1:8000/api/resturants').then((risp) =>{
-                store.resturants = risp.data.response;
+           if(this.store.resturants === null || this.store.resturants === undefined){
+            axios.get(this.store.basicUrl+'api/resturants').then((risp) =>{
+                this.store.resturants = risp.data.response;
+                console.log(this.store.resturants);
             })
            }
         },
@@ -25,13 +30,16 @@ export default {
 }
 </script>
 <template>
-    <div class="row">
+    <div class="row mt-5">
         <!--ALTERNATIVA AL BOTTONE SEARCH: @keyup="getResturants(search)" -->
-        <div class="col-6 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" >
+        <div class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" >
+            <label for="" class="form-label text-white">Cerca tra i ristoranti:</label>
             <input type="search" class="form-control" placeholder="Search..." aria-label="Search" v-model="search">
         </div>
-        <div v-for="resturant in store.resturants" :key="resturant.id">
-            <h1 v-if="resturant.name.toLowerCase().includes(search.toLowerCase())">{{ resturant.name }}</h1>
+        <div class="row">
+            <div class="col-6 g-5" v-for="resturant in store.resturants" :key="resturant.id" v-show="resturant.name.toLowerCase().includes(search.toLowerCase())">
+                <ResturantCard :resturant="resturant"/>
+            </div>
         </div>
     </div>
 </template>
