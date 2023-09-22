538d3e9cfc571638df2a428c9a686d166da7d391
S-Lombardi <silo.lombardi@gmail.com>
2023-09-22 09:07:48 +0200

    Modifiche front-end

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,19 +4,19 @@ export default {
 }
 </script>
 <template>
-    <header class="p-3 text-bg-dark">
+    <header class="p-3 bg-white ">
         <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><router-link :to="'/'" class="nav-link px-2 text-white">Home</router-link></li>
-                    <li><router-link :to="'/resturants'" class="nav-link px-2 text-white">Ristoranti</router-link></li>
-                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            <div class=" d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="text-dark nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><router-link :to="'/'" class="nav-link px-2 ">Home</router-link></li>
+                    <li><router-link :to="'/resturants'" class="nav-link px-2 ">Ristoranti</router-link></li>
+                    <li><a href="#" class="nav-link px-2">FAQs</a></li>
+                    <li><a href="#" class="nav-link px-2">About</a></li>
                 </ul>
         
                 <div class="text-end">
-                    <span class="text-white">Sei un ristoratore?</span>
-                    <a type="button" class=" text-white mx-3" href="http://127.0.0.1:8000/login">Accedi</a>
+                    <span class="text-dark">Sei un ristoratore?</span>
+                    <a type="button" class=" text-dark mx-3" href="http://127.0.0.1:8000/login">Accedi</a>
                     <a type="button" class="btn btn-warning"  href="http://127.0.0.1:8000/register">Registrati</a>
                 </div>
             </div>
