a606875c994329a4cd05047cb80ede845cb283a6
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-25 12:40:23 +0200

    modifiche checkout

--- src/components/BrainTree.vue
+++ src/components/BrainTree.vue
@@ -3,6 +3,9 @@ import 'https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.js';
 import axios from 'axios';
 import {store} from '../store'
 export default {
+    props:{
+        form: Object
+    },
     data() {
         return {
             axios,
@@ -14,51 +17,54 @@ export default {
     },
     mounted() {
         this.getCart(this.$route.params.cart);
-        this.createCheckoutForm();
     },
     methods: {
         createCheckoutForm(){
             let new_cart = this.cart;
             let rest_id = this.$route.params.cart;
-            this.axios.get(this.store.basicUrl+'api/payments/token').then((risp)=>{
-                this.clientToken = risp.data.response.clientToken;
-                let button = document.querySelector('#submit-button');
-                braintree.dropin.create({
-                    authorization: this.clientToken,
-                    selector: '#dropin-container'
-                }, function (err, instance) {
-                    if(err){
-                        //errore
-                    }else{
-                        button.addEventListener('click', function () {
-                            instance.requestPaymentMethod(function (err, payload) {
-                                if(err){
-                                    //errore
-                                }else{
-                                    axios.post(store.basicUrl+'api/payments/process', {
-                                        'nonce' : payload.nonce,
-                                        'cart' :new_cart,
-                                        'order': {
-                                            'resturant_id' : rest_id,
-                                            'address': 'Via Gaudi, 5',
-                                            'costumer_email' : 'costumer'+rest_id+'@mail.com',
-                                            'costumer_name': 'Piero',
-                                            'costumer_surname': 'Pierozzi',
-                                            'delivery_time' : '22:50:00',
-                                            'total_price': '20.00'
-                                        }
-                                    }).then((risp)=>{
-                                        console.log(risp.data.response);
-                                        localStorage.removeItem(rest_id);
-                                    }).catch((error) => {
+            if (this.form['name'] && this.form['surname'] && this.form['email'] && this.form['address'] && this.form['deliverytime'] && this.form) {
+                this.axios.get(this.store.basicUrl+'api/payments/token').then((risp)=>{
+                    this.clientToken = risp.data.response.clientToken;
+                    let button = document.querySelector('#submit-button');
+                    braintree.dropin.create({
+                        authorization: this.clientToken,
+                        selector: '#dropin-container'
+                    }, function (err, instance) {
+                        if(err){
+                            //errore
+                        }else{
+                            button.addEventListener('click', function () {
+                                instance.requestPaymentMethod(function (err, payload) {
+                                    if(err){
                                         //errore
-                                    })
-                                }
-                            });
-                        })
-                    }
-                });
-            })
+                                    }else{
+                                        axios.post(store.basicUrl+'api/payments/process', {
+                                            'nonce' : payload.nonce,
+                                            'cart' :new_cart,
+                                            'order': {
+                                                'resturant_id' : rest_id,
+                                                'address': 'Via Gaudi, 5',
+                                                'costumer_email' : 'costumer'+rest_id+'@mail.com',
+                                                'costumer_name': 'Piero',
+                                                'costumer_surname': 'Pierozzi',
+                                                'delivery_time' : '22:50:00',
+                                                'total_price': '20.00'
+                                            }
+                                        }).then((risp)=>{
+                                            console.log(risp.data.response);
+                                            localStorage.removeItem(rest_id);
+                                        }).catch((error) => {
+                                            //errore
+                                        })
+                                    }
+                                });
+                            })
+                        }
+                    });
+                })
+            } else {
+                this.errorMessage = 'Inserisci i tuoi dati prima di pagare.'    
+            }
         },
         getCart(cart){
             let storedCart = localStorage.getItem(cart);
@@ -71,42 +77,10 @@ export default {
 }
 </script>
 <template>
-    <h1 v-if="errorMessage">{{errorMessage}}</h1>
+    <div class="alert alert-warning" v-if="errorMessage">{{errorMessage}}</div>
+    <a href="#" @click="createCheckoutForm">Inserisci il metodo di pagamento</a>
     <div id="dropin-container"></div>
-    <button id="submit-button" class="button button--small button--green">Purchase</button>
+    <button id="submit-button" class="btn btn-primary">Purchase</button>
 </template>
 <style lang="scss" scoped>
-.button {
-    cursor: pointer;
-    font-weight: 500;
-    left: 3px;
-    line-height: inherit;
-    position: relative;
-    text-decoration: none;
-    text-align: center;
-    border-style: solid;
-    border-width: 1px;
-    border-radius: 3px;
-    //-webkit-appearance: none;
-    //-moz-appearance: none;
-    display: inline-block;
-  }
-  
-  .button--small {
-    padding: 10px 20px;
-    font-size: 0.875rem;
-  }
-  
-  .button--green {
-    outline: none;
-    background-color: #64d18a;
-    border-color: #64d18a;
-    color: white;
-    transition: all 200ms ease;
-  }
-  
-  .button--green:hover {
-    background-color: #8bdda8;
-    color: white;
-  }
 </style>
--- src/pages/AppCheckOut.vue
+++ src/pages/AppCheckOut.vue
@@ -3,12 +3,59 @@ import BrainTree from '../components/BrainTree.vue';
 export default {
     components:{
         BrainTree
-    }
+    },
+    data() {
+        return {
+            customerName: '',
+            customerSurname: '',
+            customerEmail: '',
+            adderss: '',
+            deliveryTime: ''
+        }
+    },
+    methods: {
+        dataForm(){
+            return {
+                'name' : this.customerName,
+                'surname' : this.customerSurname,
+                'email' : this.customerEmail,
+                'address' : this.adderss,
+                'deliveryTime' : this.deliveryTime
+            }
+        }
+    },
 }
 </script>
 <template>
-    <div class="container">
-        <BrainTree/>   
+    <div class="container card mt-5">
+        <h2 class="text-center mt-3">Checkout</h2>
+        <div class="row">
+            <div class="col-6 pt-4">
+                <div class="mb-3">
+                    <label class="form-label">Nome</label>
+                    <input type="text" class="form-control" v-model="customerName">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Cognome</label>
+                    <input type="text" class="form-control" v-model="customerSurname">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="email" class="form-control" v-model="customerEmail">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Indirizzo</label>
+                    <input type="text" class="form-control" v-model="adderss">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Orario</label>
+                    <input type="time" class="form-control" v-model="deliveryTime">
+                </div>
+            </div>
+            <div class="col-6">
+                <BrainTree :form="dataForm()"/>   
+            </div>
+        </div>
     </div>
 </template>
 <style lang="scss">
