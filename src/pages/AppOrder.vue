8588f7b7f12b567cd6303ad969ff48f68bf9174a
SimoneVilardo <simovila95@gmail.com>
2023-09-28 10:56:56 +0200

    nuova pagina per il riepilogo ordine

--- src/pages/AppOrder.vue
+++ src/pages/AppOrder.vue
@@ -8,6 +8,7 @@
         <div class="row text-center text-white">
             <h2>Grazie per aver scelto il nostro sito, il tuo pagamento è andato a buon fine! <i class="fa-solid fa-circle-check" style="color: #11d432;"></i></h2>
             <h3>Qui trovi il riepilogo del tuo ordine</h3>
+            
         </div>
     </div>
 </template>
