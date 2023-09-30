2536f145fb6c930f693a92955c04beb8e4e6fe9f
S-Lombardi <silo.lombardi@gmail.com>
2023-09-30 19:54:11 +0200

    inserito div per le classi delle card

--- src/components/ResturantCard.vue
+++ src/components/ResturantCard.vue
@@ -13,33 +13,39 @@ export default {
 </script>
 
 <template>
-    <router-link :to="{name: 'menu',params: {slug: resturant.slug} }" class="card rounded-0 h-100 text-decoration-none" style="width: 100%;" >
-        <div class="position-relative w-100"> 
-            <!-- IMMAGINE RISTORANTE -->
-            <img class="img-fluid w-100" v-if="resturant.cover_image === null" src="../assets/img/default-vite.png" >
-    
-            <!-- IMMAGINE DI DEFAULT RISTO. -->
-            <img class="img-fluid w-100"   v-else :src="`${store.basicUrl}storage/${resturant.cover_image}`" > 
+    <router-link :to="{name: 'menu',params: {slug: resturant.slug} }" >
 
-        </div>
-        <div class="card-body m-4">
-            <!-- ICONA TIPOLOGIA CUCINA -->
-            <span v-for="type in resturant.types" :key="type.id">
-                <i :class="['fas', type.icon]" class="me-5 fs-3"></i>
-            </span>
-            <!-- NOME RISTO. -->
-            <h5 class="text-primary mt-4">
-                {{ resturant.name }}
-            </h5>
-            <!-- VIA RISTO. -->
-            <p class="card-text">
-                <i class="fa-solid fa-location-dot text-warning"></i>
-                {{ resturant.address }}
-            </p>
-            <!-- TIPOLOGIA RISTO. -->
-            <span class="badge bg-primary me-1" v-for="type in resturant.types" :key="type.id">
-            {{ type.name }}
-            </span>
+        <div class="card rounded-0 h-100 text-decoration-none" style="width: 100%;">
+
+            <div class="position-relative w-100"> 
+                <!-- IMMAGINE RISTORANTE -->
+                <img class="img-fluid w-100" v-if="resturant.cover_image === null" src="../assets/img/default-vite.png" >
+        
+                <!-- IMMAGINE DI DEFAULT RISTO. -->
+                <img class="img-fluid w-100"   v-else :src="`${store.basicUrl}storage/${resturant.cover_image}`" > 
+
+            </div>
+            <div class="card-body m-4">
+                <!-- ICONA TIPOLOGIA CUCINA -->
+                <span v-for="type in resturant.types" :key="type.id">
+                    <i :class="['fas', type.icon]" class="me-5 fs-3"></i>
+                </span>
+                <!-- NOME RISTO. -->
+                <h5 class="text-primary mt-4">
+                    {{ resturant.name }}
+                </h5>
+                <!-- VIA RISTO. -->
+                <p class="card-text">
+                    <i class="fa-solid fa-location-dot text-warning"></i>
+                    {{ resturant.address }}
+                </p>
+                <!-- TIPOLOGIA RISTO. -->
+                <span class="badge bg-primary me-1" v-for="type in resturant.types" :key="type.id">
+                {{ type.name }}
+                </span>
+            </div>
+
+
         </div>
 
     </router-link>
